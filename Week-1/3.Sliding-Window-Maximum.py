5e8bb0f36dbafda2b6ec5342484dd7862dbbaa88
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-10-07 17:46:28 -0300

    naive solution - time limit exceeded

--- Week-1/3.Sliding-Window-Maximum.py
+++ Week-1/3.Sliding-Window-Maximum.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        maxs = []
+        for i in range(len(nums)-k+1):
+            maxv = max(nums[i:i+k])
+            maxs.append(maxv)
+        return maxs
