2fbc76ed05b56224e65fb75eabd18f1c29bf81a5
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-10-10 12:56:50 -0300

    Update 5.Plus-One.py

--- Extras/5.Plus-One.py
+++ Extras/5.Plus-One.py
@@ -13,3 +13,16 @@ class Solution:
         for c in temp:
             res.append(int(c))
         return res
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits)-1
+        while i>=0:
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+            i-=1
+        return [1] + digits
+        
