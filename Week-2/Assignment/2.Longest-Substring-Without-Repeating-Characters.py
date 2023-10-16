03caafc9bad17bbc2a12405a59d1146e4ab1db6a
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-10-16 02:17:29 -0300

    O(N^2)

--- Week-2/Assignment/2.Longest-Substring-Without-Repeating-Characters.py
+++ Week-2/Assignment/2.Longest-Substring-Without-Repeating-Characters.py
@@ -1,1 +1,15 @@
-
+# failing a single test-case
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substrings = []
+        if len(s)==0:
+            return 0
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                substrings.append(s[i:j])
+        unique_strs = []
+        for substr in substrings:
+            if len(substr) == len(set(substr)):
+                unique_strs.append(substr)
+        temp = sorted(unique_strs,key=(lambda x: len(x)),reverse=True)
+        return len(temp[0])  
