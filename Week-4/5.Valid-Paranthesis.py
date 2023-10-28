f496135de1eb6b35e3dae337478f59ec60bd1b97
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-10-28 15:25:30 -0300

    T:O(N) and S:O(N) using stacks

--- Week-4/5.Valid-Paranthesis.py
+++ Week-4/5.Valid-Paranthesis.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        lookup = {'}':'{',']':'[',')':'('}
+        
+        for char in s:
+            if char in lookup.values():
+                stack.append(char)
+            elif stack and lookup[char] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+        
+        return stack == []
