9412f55fe1896a8f6fe30341e9159769a43fce73
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-12-02 20:31:00 -0400

    Create 452.Min-Number-of-Arrows-to-Burst-Ballons.py

---
+++ Week-7/452.Min-Number-of-Arrows-to-Burst-Ballons.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMinArrowShots(self, points: list[list[int]]) -> int:
+
+        points.sort(key = lambda x: x[1])     
+                                              
+        tally, bow = 1, points[0][1]
+                                              
+        for start, end in points:             
+            if bow < start:                   
+                bow = end                    
+                tally += 1                   
+                                             
+        return tally                          
